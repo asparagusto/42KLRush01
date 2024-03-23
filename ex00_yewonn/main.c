b76fed7b344286dcb8d3f9e7d477662fd5cf945d
Yew Onn Woo <yewoo@u81z06s04.42kl.edu.my>
2024-03-23 12:48:12 +0800

    Initial framework-Further breakdown needed

--- ex00_yewonn/main.c
+++ ex00_yewonn/main.c
@@ -11,4 +11,11 @@ int	main(int argc, char **argv)
 	if (argc != 2)
 		ft_putstr("Wrong input: ./rush-01 \"input\"\n");
 	argv [1] = "hello";
+
+	/*1. check the format of the intended input, if format not accepted, print "Error\n" */
+			/*1. a. format of input */
+			/*1. b. No solutions. */
+
+
+/* Program output logic */
 }
